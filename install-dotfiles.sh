cf107ae66d7ebd57db30e0da83bd81bebcc7cd8c
Harrison Ng <tplawyer@gmail.com>
2018-03-17 20:39:07 +0800

    iterm2 integration

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -2,6 +2,9 @@
 
 cp .vimrc.after $HOME/.vimrc.after
 cp .vimrc.before $HOME/.vimrc.before
+
+curl -L https://iterm2.com/shell_integration/zsh \
+     -o ~/.iterm2_shell_integration.zsh
 cp .zshrc $HOME/.zshrc
 
 vim +PluginInstall +qall
