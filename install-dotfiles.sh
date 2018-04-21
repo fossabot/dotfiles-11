26bc19284b5c509585282ee37fd5bb808615f78a
Harrison Ng <tplawyer@gmail.com>
2018-04-21 15:09:39 -0700

    use https instead

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -7,7 +7,7 @@ curl -L https://iterm2.com/shell_integration/zsh \
      -o ~/.iterm2_shell_integration.zsh
 cp .zshrc $HOME/.zshrc
 
-git clone git@github.com:stulzer/heroku-colorscheme.git $HOME/.vim/janus/vim/colors/heroku-colorscheme
+git clone https://github.com/stulzer/heroku-colorscheme.git $HOME/.vim/janus/vim/colors/heroku-colorscheme
 
 vim +PluginInstall +qall
 vim +PluginUpdate +qall
